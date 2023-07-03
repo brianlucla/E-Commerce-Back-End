bfce2ffdf9d743a8390abae093e3af9114642a8e
brianlu18 <brianlu18@g.ucla.edu>
2023-07-03 01:42:56 -0500

    wrote up all the properties for the models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,35 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true,
+    },
+    product_name:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    price:{
+      type:DataTypes.DECIMAL,
+      allowNull:false,
+      // why no work
+      validate:isDecimal,
+    },
+    stock:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      // why no work
+      validate: isIn,
+    }
+    category_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:'category',
+        key:'id',
+      }
+    }
   },
   {
     sequelize,
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{Model:Category}, {Model:Tag, through:ProductTag, as:'products_tagged'}]
+    });
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findOne(req.params.id, {
+      include: [{Model:Category}, {Model:Tag, through:ProductTag, as:'products_tagged'}]
+    });
+    res.status(200).json(productData);
+  } catch (error) {
+    
+  }
 });
 
 // create new product
@@ -94,6 +110,12 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({where:{id:req.params.id}});
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 module.exports = router;
